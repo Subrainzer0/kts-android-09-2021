3f9c3141bd33ae2ba79f311acf92f9d010829cff
ShaninAA <shanin-aa@roxiemobile.com>
2021-09-19 03:02:28 +0300

    hw-2 Added full path for layout from R class

--- app/src/main/java/ru/shanin/madarareddit/ui/onboarding/OnBoardingFragment.kt
+++ app/src/main/java/ru/shanin/madarareddit/ui/onboarding/OnBoardingFragment.kt
@@ -5,10 +5,10 @@ import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import by.kirich1409.viewbindingdelegate.viewBinding
-import ru.shanin.madarareddit.R.layout
+import ru.shanin.madarareddit.R
 import ru.shanin.madarareddit.databinding.FragmentOnBoardingBinding
 
-class OnBoardingFragment : Fragment(layout.fragment_on_boarding) {
+class OnBoardingFragment : Fragment(R.layout.fragment_on_boarding) {
 
     private val binding: FragmentOnBoardingBinding by viewBinding()
 
